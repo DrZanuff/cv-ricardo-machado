32436fe3a05eb84fd66d3a0f0c5a0382aba8fc54
Ricardo <ricardo.machado.nwi@gmail.com>
2022-03-08 16:55:34 -0300

    Switch language context base by user language

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from 'react'
+import { useRecoilState } from 'recoil'
+import { language } from '../src/atoms'
 import { SideBarMenu } from '../src/components/SideBarMenu'
 import { Sections } from '../src/components/Sections'
 import { LanguageContext } from '../src/components/LanguageContext'
 import * as S from '../styles/home'
 
 export default function Home() {
+  const [, setCurrentLanguage] = useRecoilState(language)
+
+  useEffect(() => {
+    const userLang = navigator.language || 'enUS'
+
+    if (userLang === 'pt-BR') {
+      setCurrentLanguage('ptBR')
+    }
+  }, [])
+
   return (
     <S.Container>
       <LanguageContext />
